f54f8d5ba813ce954498052031ce55171243cd35
amitsharma1992 <amit.sharma@monoxor.com>
2015-07-28 05:33:41 -0700

    inventory beans made

---
+++ src/inventory/BeanProduct.java
@@ -0,0 +1,14 @@
+package inventory;
+
+public class BeanProduct 
+{
+	private int productId;
+	
+	private String productSku,skuReplica,productName,statusListing;
+	public BeanProduct() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
