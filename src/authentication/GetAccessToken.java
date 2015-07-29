0f2bd41883298367b6e9d5f513f65c67ab898213
PankajMoolrajani <pmool@nyu.edu>
2015-07-29 00:27:59 -0400

    create: GetAccessToken - createUserSession()

--- src/authentication/GetAccessToken.java
+++ src/authentication/GetAccessToken.java
@@ -1,10 +1,12 @@
 package authentication;
 
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.security.SecureRandom;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -21,7 +23,8 @@ import authentication.BeanUserCredentials;
 
 @Path("/authentication/get-access-token")
 public  class GetAccessToken  {
-
+	
+	String access_token;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -29,47 +32,62 @@ public  class GetAccessToken  {
     public String getAccessToken(BeanUserCredentials bean_user_credentials) {
         String username = bean_user_credentials.getUsername();
         String password = bean_user_credentials.getPassword();
-        this.veifyUserCredentials(username, password);
-		String access_token = "pankaj - access - token"+username+" "+password;
+        boolean ps_result = this.veifyUserCredentials(username, password);
+        if (ps_result){
+			
+			this.createUserSession();
+			access_token = this.createAccessToken();
+			System.out.println(access_token);
+			
+		}
+        
         return access_token;
         
     }	
 
     public boolean veifyUserCredentials(String username, String password) {
         
-    	Connection con = db.DbConnection.getConnection();
-    	String table_name = "user";
-    	String column_name = "id_pass";
-    	String condition = "name_user";
+    	boolean ps_result = false;
     	
-    	String query = "SELECT "+column_name+" FROM "+table_name+" WHERE "+condition+" = ?";
+    	String table_name_user = "user";
+    	String column_name_user = "id_pass";
+    	String condition_user = "name_user";
+    	String query_user = "SELECT "+column_name_user+" FROM "+table_name_user+" WHERE "+condition_user+" = ?";
+    	
+    	String table_name_pass = "pass";
+    	String column_name_pass = "password";
+    	String condition_pass = "( "+query_user+" )";
+    	String query_pass = "SELECT "+column_name_pass+" FROM "+table_name_pass+" WHERE "+condition_pass;
+    	
+    	String query = query_pass;
     	
     	try{
+    		
+        	Connection con = db.DbConnection.getConnection();
     		PreparedStatement ps = con.prepareStatement(query);
     		ps.setString(1, username);
-    		Boolean ps_result = ps.execute();
-    		System.out.println(ps_result);
+    		ps_result = ps.execute();
     		
-    		if (true){
-    			this.createUserSession();
-    			this.createAccessToken();
-    		}
     	} 
     	catch(SQLException e){
     		e.printStackTrace();
     	}
-        return false;
+    	
+        return ps_result;
         
     }	
 
     public void createUserSession() {
-        //crate user session
+        //create user session
         
     }	
 
     public String createAccessToken() {
-        //create with SecureRandomClass
-        return null;
+    	
+    	SecureRandom random = new SecureRandom();
+    	String access_token = new BigInteger(130, random).toString(32);
+    	
+        return access_token;
         
     }	
 
