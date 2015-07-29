6e711059c420c7fd8bd6bb33eb6ce6e0de3a0a13
PankajMoolrajani <pmool@nyu.edu>
2015-07-28 23:47:15 -0400

    update: GetAccessToken - verifyUserCredentials()

--- src/authentication/GetAccessToken.java
+++ src/authentication/GetAccessToken.java
@@ -27,22 +27,43 @@ public  class GetAccessToken  {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
     public String getAccessToken(BeanUserCredentials bean_user_credentials) {
-        System.out.println("got request");
         String username = bean_user_credentials.getUsername();
         String password = bean_user_credentials.getPassword();
+        this.veifyUserCredentials(username, password);
 		String access_token = "pankaj - access - token"+username+" "+password;
         return access_token;
         
     }	
 
-    public boolean veifyUserCredentials(String username, boolean password) {
+    public boolean veifyUserCredentials(String username, String password) {
         
+    	Connection con = db.DbConnection.getConnection();
+    	String table_name = "user";
+    	String column_name = "id_pass";
+    	String condition = "name_user";
+    	
+    	String query = "SELECT "+column_name+" FROM "+table_name+" WHERE "+condition+" = ?";
+    	
+    	try{
+    		PreparedStatement ps = con.prepareStatement(query);
+    		ps.setString(1, username);
+    		Boolean ps_result = ps.execute();
+    		System.out.println(ps_result);
+    		
+    		if (true){
+    			this.createUserSession();
+    			this.createAccessToken();
+    		}
+    	} 
+    	catch(SQLException e){
+    		e.printStackTrace();
+    	}
         return false;
         
     }	
 
     public void createUserSession() {
-        
+        //crate user session
         
     }	
 
