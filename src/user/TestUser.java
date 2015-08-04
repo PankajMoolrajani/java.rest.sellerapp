d9d6e839dc5d0c48e54d74974a02d0000add53b2
amitsharma1992 <amit.sharma@monoxor.com>
2015-08-04 06:18:08 -0700

    changes in user create

---
+++ src/user/TestUser.java
@@ -0,0 +1,23 @@
+package user;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TestUser
+{
+	public static void main(String ar[])
+	{
+		Map<String,String> ob = new HashMap<String,String>();
+		
+		
+		Map<String,Object> ob1 = new HashMap<String,Object>();
+		ob1.put("amit1", "Sharma1");
+	}
+}
