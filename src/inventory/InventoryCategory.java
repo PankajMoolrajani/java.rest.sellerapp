5aa6a1dfde801bfaca66e2728ad998971862c30a
PankajMoolrajani <pmool@nyu.edu>
2015-08-14 18:53:36 -0400

    update: InvetnroyCategory.java - getInventory()

--- src/inventory/InventoryCategory.java
+++ src/inventory/InventoryCategory.java
@@ -2,17 +2,22 @@
 
 package inventory;
 
+import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
 
 import inventory.BeanInventoryCategory;
 import db.DbConnection;
@@ -26,41 +31,68 @@ public  class InventoryCategory  {
 	@Produces(MediaType.TEXT_PLAIN)
 	
     public String createInventoryCategory(BeanInventoryCategory bean_inventory_category) {
-        
-		System.out.println(bean_inventory_category.getId());
-		System.out.println(bean_inventory_category.getIdParentCategory());
-		System.out.println(bean_inventory_category.getName());
-		System.out.println(bean_inventory_category.getNameTable());
+		
+		Map <String,Object> map = new HashMap<String,Object>();
+		
+		int id = 0;
 		
 		if (checkExistence(bean_inventory_category) == false){
-			this.create(bean_inventory_category);
+			
+			id = this.create(bean_inventory_category);
+			
+			if (id != 0){
+	
+				map.put("id", id);
+				
+				map.put("response_code", 2000);
+				map.put("response_message", "success: create category");
+				
+				return new Gson().toJson(map);
+				
+			}
+			else {
+				
+				map.put("id", 0);
+				
+				map.put("response_code", 4000);
+				map.put("response_message", "failure: create category");
+				
+				return new Gson().toJson(map);
+				
+			}
+			
 		}
-        return null;
+		else {
+			
+			map.put("response_code", 4000);
+			map.put("response_message", "category already exists");
+			
+			return new Gson().toJson(map);
+		}
         
     }	
 	
-	String create(BeanInventoryCategory bean_inventory_category){
-		
+	int create(BeanInventoryCategory bean_inventory_category){
+				
 		int id = 0;
 		
 		String table_name = "inventory_category";
 		String columns = "id_parent_category, name, name_table, id_tax";
 		String values = "?, ?, ?, ?";
 		String query = "INSERT INTO "+table_name+"("+columns+")"+" VALUES "+"("+values+")";
-		System.out.println(query);
+		
 		Connection con = DbConnection.getConnection();
 		
 		try {
 			
 			PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			
 			ps.setInt(1, bean_inventory_category.getIdParentCategory());
 			ps.setString(2, bean_inventory_category.getName());
 			ps.setString(3, bean_inventory_category.getNameTable());
 			ps.setInt(4, bean_inventory_category.getIdTax());
-			System.out.println(ps);
 			
 			int rows_affected =  ps.executeUpdate();
+			
 			if (rows_affected != 0){
 				
 				ResultSet rs = ps.getGeneratedKeys();
@@ -68,7 +100,6 @@ public  class InventoryCategory  {
 				if (rs.next()){
 				
 					id = rs.getInt(1);
-					System.out.println(id);
 					
 				}    			
 			}
@@ -77,7 +108,7 @@ public  class InventoryCategory  {
 		catch (SQLException e){
 			e.printStackTrace();
 		}
-		return null; 
+		return id; 
 	}
 	
 	Boolean checkExistence(BeanInventoryCategory bean_inventory_category){
@@ -122,8 +153,18 @@ public  class InventoryCategory  {
         
     }	
 
+    
+    @POST
+	@Path("/get")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+    
     public String getInventory(Object identifier) {
-        
+    	
+    	//convert Object identifier to map - amit
+    	
+    	System.out.println(identifier);
+    	System.out.println(identifier.getClass().getName());
         return null;
         
     }	
